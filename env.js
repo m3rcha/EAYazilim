3a69cfab0fc91c4b405ed1b53fcca24fdd807c71
m3rcha <egezten@gmail.com>
2026-05-04 15:05:08 +0300

    style: reformat HTML markup for improved readability and consistency

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 // env.js
 window.ENV = {
-    SUPABASE_URL: 'https://supabase.eayazilim.tr',
-    SUPABASE_ANON_KEY: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3NzU3ODQyMCwiZXhwIjo0OTMzMjUyMDIwLCJyb2xlIjoiYW5vbiJ9.d7TsjQweAnO2lny2liV7FGJwEzQMdI_JT_16Nm9XJ_c',
+    SUPABASE_URL: 'https://supabasekong.eayazilim.tr',
+    SUPABASE_ANON_KEY: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3Nzg4ODgwMCwiZXhwIjo0OTMzNTYyNDAwLCJyb2xlIjoiYW5vbiJ9.RmoyALihv6ISUWR9csocj1xMpgro8Sw5IFIkyR1dQ-E',
     GA_TRACKING_ID: 'G-XXXXXXXXXX'
 };
