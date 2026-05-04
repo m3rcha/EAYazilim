072f95c4e9fb99308eb7aa70d3be96fe778db80f
m3rcha <egezten@gmail.com>
2026-05-04 20:23:46 +0300

    feat: integrate Google Analytics tracking across all pages and update content for clarity

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 // env.js
 window.ENV = {
     SUPABASE_URL: 'https://supabasekong.eayazilim.tr',
-    SUPABASE_ANON_KEY: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3Nzg4ODgwMCwiZXhwIjo0OTMzNTYyNDAwLCJyb2xlIjoiYW5vbiJ9.RmoyALihv6ISUWR9csocj1xMpgro8Sw5IFIkyR1dQ-E',
-    GA_TRACKING_ID: 'G-XXXXXXXXXX'
+    SUPABASE_ANON_KEY: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3Nzg4ODgwMCwiZXhwIjo0OTMzNTYyNDAwLCJyb2xlIjoiYW5vbiJ9.RmoyALihv6ISUWR9csocj1xMpgro8Sw5IFIkyR1dQ-E'
+    GA_TRACKING_ID: 'G-K2JMKDJMRL'
 };
